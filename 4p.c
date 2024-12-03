46f565f25cc34c5ef418b083a0d6e61e1823e611
Jai Pranesh <jai202966@gmail.com>
2024-12-03 11:40:13 +0530

    Create 4p.c

---
+++ 4p.c
@@ -0,0 +1,14 @@
+#include <stdio.h> #include<conio.h>
+void main()
+{
+clrscr();
+for (int i=5; i>0; i--)
+{
+for (int j=i; j>0; j--)
+printf();
+for (j=0; j<i; j++)
+printf();
+printf("\n");
+}
+getch();
+{
